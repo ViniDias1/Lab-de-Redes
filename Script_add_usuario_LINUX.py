95a7f725615c206017c8672c6ee96fadcac99455
ViniDias <90332822+ViniDias1@users.noreply.github.com>
2024-03-04 22:10:35 -0300

    Update Script_add_usuario_LINUX.py

--- Script_add_usuario_LINUX.py
+++ Script_add_usuario_LINUX.py
@@ -1,47 +1,18 @@
 
-import os 
+
 import sys
-import subprocess 
-import getpass 
-import platform 
+import os
 import crypt
 
-usuario = input ("Nome de usuario: ")
-diretorio = input("Diretorio: ")
-grupo = input("Grupo: ")
-senha = (crypt.crypt(getpass.getpass("Senha: "), crypt.mksalt(crypt.METHOD_SHA512)))  
+def adicionarUsuario(nome):
+    #try:
+        os.system("useradd "+nome)
+        os.system("passwd "+nome)
+    #except:
+        #print("Erro")
+        #sys.exit(1)
+nome = sys.argv[1]
+#senha = sys.argv[2]
 
-
-# adicionar grupo
-def novogrupo():
-    try:
-        subprocess.run(['groupadd', grupo ])
-
-    except: 
-         print("Erro ao criar grupo")                      
-         sys.exit(1)
-
-# criar diretorio
-def novodir():
-    try:
-        # rodar o comando mkdir do Linux 
-        subprocess.run(['mkdir', "-p", diretorio ])
-
-    except: 
-         print("Erro ao criar diretorio")                      
-         sys.exit(1) 
-
-# adicionar o usuario
-def novaconta(): 
-
-    try: 
-         # rodar o comando useradd do Linux
-         subprocess.run(['useradd', '-c', comentario, '-d', diretorio, '-g', grupo, '-M', '-s', '/sbin/nologin', '-p', senha, usuario ])
-
-    except: 
-         print("Erro ao criar usuario")                      
-         sys.exit(1)
-
-novogrupo()
-novodir()
-novaconta()
+adicionarUsuario(nome)
+print "Usuario criado com sucesso!" 
